2b8ba8e5f8609442473b232de524cd52d07b92a0
Vladimir Rybakov <vladimir.rybakov@gmail.com>
2025-02-08 21:43:11 +0100

    DO deploy

---
+++ run-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# Generate a tag name based on the current date and time (YYYYMMDDHHMM)
+TAG_NAME=$(date +%Y%m%d%H%M)
+
+VLANDIVIR_BOT_TOKEN=$(grep TEST_BOT_TOKEN .env | cut -d '=' -f2)
+POSTGRES_CONNECTION_STRING=$(grep POSTGRES_CONNECTION_STRING .env | cut -d '=' -f2)
+DO_SPACES_ACCESS_KEY=$(grep DO_SPACES_ACCESS_KEY .env | cut -d '=' -f2)
+DO_SPACES_SECRET_KEY=$(grep DO_SPACES_SECRET_KEY .env | cut -d '=' -f2)
+
+docker build \
+  --build-arg VLANDIVIR_BOT_TOKEN="${VLANDIVIR_BOT_TOKEN}" \
+  --build-arg TAG_NAME="${TAG_NAME}" \
+  --build-arg ENVIRONMENT="DOCKER" \
+  --build-arg POSTGRES_CONNECTION_STRING="${POSTGRES_CONNECTION_STRING}" \
+  --build-arg DO_SPACES_ACCESS_KEY="${DO_SPACES_ACCESS_KEY}" \
+  --build-arg DO_SPACES_SECRET_KEY="${DO_SPACES_SECRET_KEY}" \
+  -t vlandivir-2025 .
+
+if docker ps -a | grep -q vlandivir-2025; then
+  docker stop vlandivir-2025 || true
+  docker rm vlandivir-2025 || true
+fi
+
+docker run --name vlandivir-2025 vlandivir-2025
---
+++ run-production-deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# Generate a tag name based on the current date and time (YYYYMMDDHHMM)
+TAG_NAME=$(date +%Y%m%d%H%M)
+
+# Load the server IP address from server_ip.txt
+SERVER_IP=$(grep DIGITAL_OCEAN_IP .env | cut -d '=' -f2)
+
+# Extract VLANDIVIR_BOT_TOKEN from .env
+VLANDIVIR_BOT_TOKEN=$(grep VLANDIVIR_BOT_TOKEN .env | cut -d '=' -f2)
+POSTGRES_CONNECTION_STRING=$(grep POSTGRES_CONNECTION_STRING .env | cut -d '=' -f2)
+DO_SPACES_ACCESS_KEY=$(grep DO_SPACES_ACCESS_KEY .env | cut -d '=' -f2)
+DO_SPACES_SECRET_KEY=$(grep DO_SPACES_SECRET_KEY .env | cut -d '=' -f2)
+
+docker build \
+  --build-arg VLANDIVIR_BOT_TOKEN="${VLANDIVIR_BOT_TOKEN}" \
+  --build-arg TAG_NAME="${TAG_NAME}" \
+  --build-arg ENVIRONMENT="PROD" \
+  --build-arg POSTGRES_CONNECTION_STRING="${POSTGRES_CONNECTION_STRING}" \
+  --build-arg DO_SPACES_ACCESS_KEY="${DO_SPACES_ACCESS_KEY}" \
+  --build-arg DO_SPACES_SECRET_KEY="${DO_SPACES_SECRET_KEY}" \
+  -t vlandivir-2025 .
+
+docker tag vlandivir-2025 registry.digitalocean.com/vlandivir-main/vlandivir-2025:$TAG_NAME
+docker push registry.digitalocean.com/vlandivir-main/vlandivir-2025:$TAG_NAME
+
+# Update the Docker container on the DigitalOcean droplet
+SSH_COMMANDS="docker pull registry.digitalocean.com/vlandivir-main/vlandivir-2025:$TAG_NAME; \
+docker stop vlandivir-2025; \
+docker rm vlandivir-2025; \
+docker run -d -p 80:3000 --name vlandivir-2025 registry.digitalocean.com/vlandivir-main/vlandivir-2025:$TAG_NAME"
+
+ssh -o StrictHostKeyChecking=no root@$SERVER_IP "$SSH_COMMANDS"
